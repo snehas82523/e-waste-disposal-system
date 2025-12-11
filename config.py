ab705d9e0e029647151da26e3307cb008210eb52
snehas82523 <20082523@mydbs.ie>
2025-12-11 19:42:02 +0000

    reinitialize project structure

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_key_very_secret'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
