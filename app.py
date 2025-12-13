a62953ac097b8794a887efc2525d1370b22924f7
snehas82523 <20082523@mydbs.ie>
2025-12-13 15:14:49 +0000

    routes updated

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app.config['SECRET_KEY'] = 'dev_key'
 
 @app.route('/')
 def home():
-    return "<h1>Hello! The E-Waste System is Running.</h1>"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
