8e476e97289114dc3108d797faae75e19868c28c
snehas82523 <20082523@mydbs.ie>
2025-12-13 20:38:28 +0000

    routes updated

--- app.py
+++ app.py
@@ -4,9 +4,25 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dev_key'
 
+# user page
 @app.route('/')
 def home():
     return render_template('index.html')
+
+# admin login
+@app.route('/admin/login')
+def admin_login():
+    return render_template('admin_login.html')  
+
+# admin dashboard
+@app.route('/admin')
+def admin_dashboard():
+    return render_template('admin.html')  
+
+# employee
+@app.route('/employee')
+def employee_portal():
+    return render_template('employee_portal.html')  
 
 if __name__ == '__main__':
     app.run(debug=True)
