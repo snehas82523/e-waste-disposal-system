958303e82ed58f32e261be2375a318dc1ef4bfca
snehas82523 <20082523@mydbs.ie>
2025-12-13 23:01:41 +0000

    updated js for page dectection

--- static/js/app.js
+++ static/js/app.js
@@ -11,4 +11,14 @@ document.addEventListener('DOMContentLoaded', () => {
     if (isEmployee) setupEmployeePortal();
 });
 
+function setupIndex() {
+    console.log("Index page loaded");
+}
 
+function setupAdmin() {
+    console.log("Admin page loaded");
+}
+
+function setupEmployeePortal() {
+    console.log("Employee portal loaded");
+}
