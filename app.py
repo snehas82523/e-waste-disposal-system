5bd3c7500dadb354c3ab71c9cf4647ebb9d6dca4
snehas82523 <20082523@mydbs.ie>
2025-12-13 14:21:32 +0000

    reinitialised app.py

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
-from app import create_app
-app = create_app()
+from flask import Flask, render_template
+
+# Initialize
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dev_key'
+
+@app.route('/')
+def home():
+    return "<h1>Hello! The E-Waste System is Running.</h1>"
+
 if __name__ == '__main__':
     app.run(debug=True)
